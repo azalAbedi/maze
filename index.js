d2ebf530c141e8dbc2875d5ef39c74fffea45646
Azal Abedi <azal.abedi@gmail.com>
2021-05-19 20:08:12 -0600

    draw success rectangle on the bottom right side of the maze

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ Runner.run(Runner.create(), engine); // set up the coordination of our world sta
 
 // Walls
 const walls = [
-    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
-    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }), 
-    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+    Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }), 
+    Bodies.rectangle(0, height / 2, 2, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 2, height, { isStatic: true })
 ];
 World.add(world, walls); // You can add a whole array of items at once!
 
@@ -151,3 +151,15 @@ verticals.forEach((row, rowIndex) => {
         World.add(world, wall);
     });
 });
+
+// Drawing the success goal on the bottom right of the maze, dynamically 
+const goal = Bodies.rectangle(
+    width - unitLength / 2,
+    height - unitLength / 2,
+    unitLength * 0.7,
+    unitLength * 0.7,
+    {
+        isStatic: true
+    }
+);
+World.add(world, goal);
